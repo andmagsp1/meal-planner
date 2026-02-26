2ce26f050f6535717016ea48cbba63a18de4c1f3
Andrea Magnussen <andrea.magnussen@sparebank1.no>
2026-02-26 09:45:58 +0100

    La til card

--- src/features/Recipes.tsx
+++ src/features/Recipes.tsx
@@ -1,3 +1,5 @@
+import { CardBase, TextCard } from "@sb1/ffe-cards-react";
+import { Heading3, Paragraph } from "@sb1/ffe-core-react";
 import { useQuery } from "@tanstack/react-query";
 import { getRecipes } from "../api/getRecipes.ts";
 
@@ -18,11 +20,18 @@ export function Recipes() {
   return (
     <div>
       <h2>Meals</h2>
-      <ul>
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "8px" }}>
         {data?.map((recipe) => (
-          <li key={recipe.id}>{recipe.name}</li>
+          <CardBase key={recipe.id} style={{ background: "#eaeaf6" }}>
+            <Heading3>{recipe.name}</Heading3>
+            <Paragraph>
+              {recipe.description.length > 50
+                ? recipe.description.slice(0, 100) + "..."
+                : recipe.description}
+            </Paragraph>
+          </CardBase>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
