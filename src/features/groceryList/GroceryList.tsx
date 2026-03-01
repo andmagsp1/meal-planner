2a10e41e9c5591731d6f2ccbf109db984b4f470a
Andrea Magnussen <andrea.magnussen@sparebank1.no>
2026-03-01 11:24:50 +0100

    green

--- src/features/groceryList/GroceryList.tsx
+++ src/features/groceryList/GroceryList.tsx
@@ -3,5 +3,9 @@ import { useGroceryList } from "./hooks/useGroceryList.ts";
 export function GroceryList() {
   const { items, isLoading, isError } = useGroceryList();
 
+  if (isLoading) {
+    return <div>Laster...</div>;
+  }
+
   return <div></div>;
 }
